6c751cb48505dc5da740debb0a8a0dc9815fe53b
Rodion Gorkovenko <rodiongork@github.com>
2015-07-17 09:40:19 +0300

    First prototypical version

---
+++ README.md
@@ -0,0 +1,3 @@
+#Alexa-Monitor
+
+this is a test project to learn a bit of python and Google-App-Engine
---
+++ main.py
@@ -0,0 +1,53 @@
+import flask
+import urllib2
+import re
+import time
+from google.appengine.ext import db
+
+class LastRecord(db.Model):
+    value = db.IntegerProperty()
+
+class Record(db.Model):
+    value = db.IntegerProperty(required = True)
+    timestamp = db.IntegerProperty(required = True)
+
+app = flask.Flask(__name__)
+app.config['DEBUG'] = False
+
+@app.route('/')
+def index():
+    return flask.render_template('index.html')
+
+@app.route('/list')
+def records():
+    q = db.GqlQuery('SELECT * FROM Record ORDER BY timestamp DESC LIMIT 30')
+    q.fetch(limit = 30)
+    res = []
+    for e in q:
+        res.append(str(e.timestamp) + ':' + str(e.value))
+    return ' '.join(res)
+
+@app.route('/capture')
+def capture():
+    resp = urllib2.urlopen('http://www.alexa.com/siteinfo/codeabbey.com')
+    page = resp.read()
+    m = re.search('ranked\snumber\s([\d\,]+)\sin\sthe\sworld', page)
+    rank = int(m.group(1).replace(',', ''))
+    q = db.GqlQuery('SELECT * FROM LastRecord')
+    lastValue = q.get()
+    if lastValue != None:
+        prevRank = lastValue.value
+    else:
+        prevRank = -1
+        lastValue = LastRecord()
+    if prevRank == rank:
+        return 'Not updated'
+    lastValue.value = rank
+    db.put(lastValue)
+    record = Record(value = rank, timestamp = int(time.time()))
+    record.put()
+    return 'New value: ' + str(rank)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return 'Sorry, nothing at this URL.', 404
